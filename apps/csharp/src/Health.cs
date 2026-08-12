19d3ec0e01157bf98db4ebad2c01fc263c76ecc5
GustavoPeixoto <gustavopcamargos@gmail.com>
2026-08-12 01:21:21 +0000

    monorepo setup

---
+++ apps/csharp/src/Health.cs
@@ -0,0 +1,12 @@
+using System.Globalization;
+
+namespace Health
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm healthy!");
+        }
+    }
+}
